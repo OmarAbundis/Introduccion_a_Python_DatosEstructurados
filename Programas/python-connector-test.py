773ea4f387bf9744ca129009650318edd4ac37fd
OmarAbundis <consulta.aomar@gmail.com>
2022-10-04 13:45:44 -0500

    Create python-connector-test.py

---
+++ Programas/python-connector-test.py
@@ -0,0 +1,11 @@
+# BIBLIOTECA
+import mysql.connector
+
+# PROGRAMA
+print ("Conectandose a la base de datos")
+cnx = mysql.connector.connect(user='OmarAb', password='kore',
+                              host='127.0.0.1',
+                              database='codigoIoT')
+print ("Conexión realizada")
+cnx.close()
+print ("Conexión cerrada")
