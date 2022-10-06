f41f3dc790521e8156b8cf0c53a2feaecdad3b4e
OmarAbundis <consulta.aomar@gmail.com>
2022-10-06 11:37:40 -0500

    Create query-rfid-mysql.py

---
+++ Programas/query-rfid-mysql.py
@@ -0,0 +1,25 @@
+# BIBLIOTECAS
+import mysql.connector
+
+#Conexión
+cnx = mysql.connector.connect(user='OmarAb', password='kore',
+                              host='127.0.0.1',
+                              database='codigoIoT')
+
+# Query
+query = ("SELECT id,nombre,temperatura FROM clima WHERE id > 21;")
+
+# Ejecución
+#cursor.execute(query)
+
+res = cnx.cmd_query(query)
+
+# Imprimir resultado
+
+print ("Respuesta")
+print (res)
+
+# Cerrar
+# cursor.close()
+
+cnx.close()
